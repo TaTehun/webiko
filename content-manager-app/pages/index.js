ffda6bf810337a0758be5f4646cf97041b10ade0
Taehun Jang <97495171+TaTehun@users.noreply.github.com>
2023-01-11 20:42:59 -0600

    Setup folder structure

---
+++ content-manager-app/pages/index.js
@@ -0,0 +1,22 @@
+
+
+function CompA(){
+  return (
+    <h1>CompA</h1>
+  )
+}
+
+
+function CompB(){
+  return (
+    <h1>CompB</h1>
+  )
+}
+
+function Home() {
+  return (
+    <h1>Hello World</h1>
+  ) 
+}
+
+export default Home;
